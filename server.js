8ed9b50b3da137f17c5d665919a7c49e6a6f46e0
vishmuk <30669538+vishmuk@users.noreply.github.com>
2017-08-22 17:11:12 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user:'vishmuk48',
+    database:'vishmuk48',
+    host:'db.imad.hasura-app.io',
+    port: '5432',
+    password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool= new Pool(config);
+app.get('/test-db',function (req, res) {
+    //Make a select request
+    //return a response with the results
+  pool.query('SELECT * FROM test', function(err,result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      }
+      else
+      {
+          res.send(JSON.stringify(result));
+      }
+});
 });
 
 app.get('/ui/style.css', function (req, res) {
