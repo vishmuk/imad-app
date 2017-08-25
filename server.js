ea27ff0792206a50d77bf7c759c30dcebbcb55ca
vishmuk <30669538+vishmuk@users.noreply.github.com>
2017-08-24 23:43:11 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ function createTemplate(data){
     <html>
     <head>
         <title>
-            ($title)
+            $(title)
         </title>
         <link href="/ui/style.css"  rel="stylesheet" />
         <meta name='viexport content = width-device-width,initial-scale=1' />
