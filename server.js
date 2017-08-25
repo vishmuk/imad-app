d53e71d8be1fed66d3d0d4357e987d62d1733b81
vishmuk <30669538+vishmuk@users.noreply.github.com>
2017-08-25 09:57:58 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,13 +89,10 @@ app.get('/:articleName',function (req,res){
     res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two',function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/article-three',function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 //app.get('/ui/madi.png', function (req, res) {
   //res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
