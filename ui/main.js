b8f401f36312639ee6bf6a8fc23ae547dbce6c6a
vishmuk <30669538+vishmuk@users.noreply.github.com>
2017-08-27 17:32:48 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ var img = document.getElementById("green");
 
 var marginLeft = 0;
 function moveRight (){
-    marginLeft = marginLeft + 10 px;
+    marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
 }
 
