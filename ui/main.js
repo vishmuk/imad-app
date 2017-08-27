6324aad099936eab9764a61d8adadc0853e66f89
vishmuk <30669538+vishmuk@users.noreply.github.com>
2017-08-27 17:15:36 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element =  document.getElementById("main-text");
 element.innerHTML= "new value tested";
 
 //move the image on click
-var img=document.getElementbyId("green");
+var im = document.getElementById("green");
 img.onclick = function (){
     img.style.marginleft='100px'
 }
