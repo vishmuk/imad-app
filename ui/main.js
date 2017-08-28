925ef5130e5d04981c8669d41808318bcedf2822
vishmuk <30669538+vishmuk@users.noreply.github.com>
2017-08-28 18:23:22 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -54,7 +54,7 @@ button.onclick = function(){
 };
 
 //submit name
-var nameInput = document.getElementById("name");
+var nameInput = document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
