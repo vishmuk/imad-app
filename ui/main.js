b2bfbebb27affc0927b4927152cde3c858d20fcc
vishmuk <30669538+vishmuk@users.noreply.github.com>
2017-08-28 18:26:38 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -62,8 +62,8 @@ submit.onclick=function(){
     //capture a list of names and render as a list
    var names = ['name1','name2','name3'];
    var list = '';
-   for (var i=0; i<names.length;i++){
-       list = '<li>'+names[i]+'<li>'; 
+   for (var i=0; i< names.length;i++){
+       list == '<li>'+names[i]+'/li>'; 
    }
   var ul =document.getElementById('namelist');
   ul.innerHTML = list;
