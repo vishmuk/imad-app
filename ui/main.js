653063d18640d2a89c4817c2459d90d8e2713f62
vishmuk <30669538+vishmuk@users.noreply.github.com>
2017-08-28 19:18:33 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,7 +42,7 @@ button.onclick = function(){
   }
   //not done yet
   
-  };
+  
   //submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
@@ -53,14 +53,14 @@ submit.onclick = function(){
     //capture a list of names and render as a list
    var names = ['name1','name2','name3','name4'];
    var list = '';
-   for (var i=0; i< names.length;i++){
+    for (var i=0; i< names.length;i++){
        list += '<li>'+names[i]+'/li>'; 
-   }
+    }
   var ul =document.getElementById('namelist');
   ul.innerHTML = list;
     
 };
-
+};
   
   //render the variable in a correct span
  // counter = counter + 1;
