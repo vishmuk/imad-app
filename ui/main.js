774796a651582a59a8f2ff2573553bcaf341ae0f
vishmuk <30669538+vishmuk@users.noreply.github.com>
2017-08-28 19:12:57 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -51,7 +51,7 @@ var submit=document.getElementById('submit_btn');
 submit.onclick = function(){
     //make a request to the server and send the name
     //capture a list of names and render as a list
-   var names = ["name1","name2","name3","name4"];
+   var names = ['name1','name2','name3','name4'];
    var list = '';
    for (var i=0; i< names.length;i++){
        list == '<li>'+names[i]+'/li>'; 
