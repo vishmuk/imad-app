0ab422bcd170ed8559d6b96fb162e3f64b29f3fc
vishmuk <30669538+vishmuk@users.noreply.github.com>
2017-08-28 21:19:16 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -54,15 +54,35 @@ var n = nameInput.value;
 var submit=document.getElementById('submit_btn');
 
 submit.onclick = function(){
-    //make a request to the server and send the name
+    var request = new XMLHttpRequest();
+  
+    //capture the response and store it in a variable
+  request.onreadystatechange = function(){
+      if (request.readyState === XMLHttpRequest.DONE) {
+      //take an action
+      if(request.status==200){
+          //make a request to the server and send the name
     //capture a list of names and render as a list
-   var names = ['name1','name2','name3','name4', 'name5'];
+   var names = request.responseText;
+   names = JSON.parse(names);
    var list = '';
     for (var i=0; i< names.length;i++){
        list += '<li>' + names[i] + '</li>'; 
     }
   var ul =document.getElementById('namelist');
   ul.innerHTML = list;
+          }
+  } 
+  //not done yet
+  
+  };
+  request.open("GET", "http://vishmuk48.imad.hasura-app.io/submit-name?name=" + n, true);
+  request.send(null);
+  
+};
+   
+   
+    
     
 };
 
