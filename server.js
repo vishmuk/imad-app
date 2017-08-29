2bd53b9251653ef2fc55b1d6d33f167a1fa176da
vishmuk <30669538+vishmuk@users.noreply.github.com>
2017-08-28 23:23:59 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,11 +125,26 @@ app.get('/submit-name', function(req,res){ //URL: /submit-name?name=xxxxx
     res.send(JSON.stringify(names));
     
 });
-app.get('/:articleName',function (req,res){
+app.get('/articles/:articleName',function (req,res){
     //articleName == article-one
     //articles[articleName] == {} content object of article one
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+    //var articleName = req.params.articleName;
+    
+    pool.query("SELECT * FROM article WHERE title= " + req.params.articleName, function(err,result) {
+      if (err)   {
+          res.status(500).send(err.toString());
+          
+      }else {
+          if(result.rows.length === 0){
+              res.status(404).send("Article Not Found");
+              }else {
+                  var articleData = result.rows[0];
+                   res.send(createTemplate(articleData));
+              }
+      }
+    });
+    
+   
 });
 
 app.get('/ui/main.js', function (req, res) {
