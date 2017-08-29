62d602a09af140fae537fd74ec3602b3e8b1ecc5
vishmuk <30669538+vishmuk@users.noreply.github.com>
2017-08-28 19:53:19 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,9 +106,9 @@ app.get('/ui/main.js', function (req, res) {
   //res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 //});
 var names = [];
-app.get('/submit-name/:name', function(req,res){
+app.get('/submit-name', function(req,res){ //URL: /submit-name?name=xxxxx
     //get the name from the request object
-    var name=req.params.name;
+    var name=req.query.name;
     names.push(name);
     //JSON: JAvascript object notation
     res.send(JSON.stringify(names));
