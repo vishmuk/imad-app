cedc5323e697ef52a315f048736b54e1c33a6905
vishmuk <30669538+vishmuk@users.noreply.github.com>
2017-08-31 21:02:29 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,6 +133,14 @@ app.post('/login' , function(req,res) {
  })  ;
 
 
+app.get('/logout', function(req,res){
+   delete req.session.auth;
+   res.send('Logged out');
+});
+
+
+
+
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     //make a select request
